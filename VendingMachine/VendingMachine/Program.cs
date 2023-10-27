3893d34610924b8131c87c8c3c791791ece4cb79
yodan <yodanlevy@mail.tau.ac.il>
2023-10-27 14:29:12 +0300

    Create validate function

---
+++ VendingMachine/VendingMachine/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace VendingMachine
+{
+    class Program
+    {
+        public class Validator
+        {
+            public static bool isValid(String str)
+            {
+                if (double.Parse(str) == 0.1 ||
+                    double.Parse(str) == 0.5 ||
+                    int.Parse(str) == 1 ||
+                    int.Parse(str) == 2 ||
+                    int.Parse(str) == 5 ||
+                    int.Parse(str) == 10)
+                {
+                    return true;
+                }
+
+                return false;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Insert coins");
+            var total = 0.0;
+            var input = Console.ReadLine();
+            while (!string.IsNullOrWhiteSpace(input))
+            {
+                if (Validator.isValid(input))
+                {
+                    total += double.Parse(input);
+                    input = Console.ReadLine();
+                }
+                else
+                {
+                    Console.WriteLine("Coin is not valid");
+                    input = Console.ReadLine();
+                }
+                
+            }
+
+            Console.WriteLine("Total: " + total);
+        }
+    }
+}
