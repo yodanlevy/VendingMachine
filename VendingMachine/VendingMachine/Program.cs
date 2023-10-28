ad5b4cd2f50d697b114bb884813efc3dbf83051e
yodan <yodanlevy@mail.tau.ac.il>
2023-10-28 20:18:31 +0300

    Extract Validator to another file

--- VendingMachine/VendingMachine/Program.cs
+++ VendingMachine/VendingMachine/Program.cs
@@ -4,24 +4,6 @@ namespace VendingMachine
 {
     class Program
     {
-        public class Validator
-        {
-            public static bool isValid(String str)
-            {
-                if (double.Parse(str) == 0.1 ||
-                    double.Parse(str) == 0.5 ||
-                    int.Parse(str) == 1 ||
-                    int.Parse(str) == 2 ||
-                    int.Parse(str) == 5 ||
-                    int.Parse(str) == 10)
-                {
-                    return true;
-                }
-
-                return false;
-            }
-        }
-
         static void Main(string[] args)
         {
             Console.WriteLine("Insert coins");
---
+++ VendingMachine/VendingMachine/Validator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+namespace VendingMachine
+{
+    public class Validator
+    {
+        static double[] _coins = { 0.1, 0.5, 1, 2, 5, 10 };
+
+        public static bool isValid(String str)
+        {
+            if (!string.IsNullOrWhiteSpace(str))
+            {
+                if (_coins.Contains(double.Parse(str)))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+
+    }
+}
