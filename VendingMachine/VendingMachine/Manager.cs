5c166c74bdfde80e84b0809d4cdf63f4b7a8ef9d
yodan <yodanlevy@mail.tau.ac.il>
2023-10-31 01:27:16 +0200

    Crate Start function

--- VendingMachine/VendingMachine/Manager.cs
+++ VendingMachine/VendingMachine/Manager.cs
@@ -1,10 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading;
 
 namespace VendingMachine
 {
-    class Class1
+    class Manager
     {
+        public void Start()
+        {
+            Console.WriteLine("Insert coins");
+            var total = 0.0;
+            var input = Console.ReadLine();
+            while (!string.IsNullOrWhiteSpace(input))
+            {
+                if (Validator.IsValid(input))
+                {
+                    total += double.Parse(input);
+                    input = Console.ReadLine();
+                }
+                else
+                {
+                    Console.WriteLine("Coin is not valid");
+                    input = Console.ReadLine();
+                }
+
+            }
+
+            Console.WriteLine("Total: " + total);
+        }
     }
 }
