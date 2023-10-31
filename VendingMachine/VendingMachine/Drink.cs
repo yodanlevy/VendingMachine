82b886986d8c5f4b0e12437c7bc62da27d53ea18
yodan <yodanlevy@mail.tau.ac.il>
2023-10-31 23:02:23 +0200

    Update Class Drink

--- VendingMachine/VendingMachine/Drink.cs
+++ VendingMachine/VendingMachine/Drink.cs
@@ -2,6 +2,16 @@
 {
     class Drink
     {
+        public string DrinkName;
+        public string SerialNumber;
+        public double Price;
+
+        public Drink(string drinkName, string sreialNumber, double price)
+        {
+            this.DrinkName = drinkName;
+            this.SerialNumber = sreialNumber;
+            this.Price = price;
+        }
 
     }
 }
