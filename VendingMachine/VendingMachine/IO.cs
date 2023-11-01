1345104f6b09558486cc67563981aea0f025268a
yodan <yodanlevy@mail.tau.ac.il>
2023-11-01 22:30:12 +0200

    Create PrintDrinks function

--- VendingMachine/VendingMachine/IO.cs
+++ VendingMachine/VendingMachine/IO.cs
@@ -1,7 +1,20 @@
-﻿namespace VendingMachine
+﻿using System;
+using System.Collections.Generic;
+
+namespace VendingMachine
 {
     public class IO
     {
+        public static DataInitializer dI = new DataInitializer();
+        public List<Drink> _drinkList = dI.InitializeDrink();
 
+        public void PrintDrinks()
+        {
+            Console.WriteLine("Please choose a drink:");
+            foreach (var VARIABLE in _drinkList)
+            {
+                Console.WriteLine(VARIABLE.SerialNumber + ". " + VARIABLE.DrinkName + " = " + VARIABLE.Price + "NIS" + "\n");
+            }
+        }
     }
 }
